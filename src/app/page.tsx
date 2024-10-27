bb8896bc3fb4d5fa300ce268c09e457d9a4dcaf4
akash <akashad1403@gmail.com>
2024-10-27 22:09:00 +0530

    add icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -134,109 +134,148 @@ const ValuesSection: React.FC = () => {
       At Everything Talent, our core values drive everything we do
     </p>
     
+   
     <div className="grid grid-cols-1 md:grid-cols-3 gap-[5rem] items-center">
+
+    <div className="flex flex-col items-center">
+      <div className="flex items-center justify-center text-2xl font-normal w-16 h-16 mb-6 bg-blue-600 rounded-xl">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="rgba(255,255,255,1)"><path d="M14.5998 8.00033H21C22.1046 8.00033 23 8.89576 23 10.0003V12.1047C23 12.3659 22.9488 12.6246 22.8494 12.8662L19.755 20.3811C19.6007 20.7558 19.2355 21.0003 18.8303 21.0003H2C1.44772 21.0003 1 20.5526 1 20.0003V10.0003C1 9.44804 1.44772 9.00033 2 9.00033H5.48184C5.80677 9.00033 6.11143 8.84246 6.29881 8.57701L11.7522 0.851355C11.8947 0.649486 12.1633 0.581978 12.3843 0.692483L14.1984 1.59951C15.25 2.12534 15.7931 3.31292 15.5031 4.45235L14.5998 8.00033ZM7 10.5878V19.0003H18.1606L21 12.1047V10.0003H14.5998C13.2951 10.0003 12.3398 8.77128 12.6616 7.50691L13.5649 3.95894C13.6229 3.73105 13.5143 3.49353 13.3039 3.38837L12.6428 3.0578L7.93275 9.73038C7.68285 10.0844 7.36341 10.3746 7 10.5878ZM5 11.0003H3V19.0003H5V11.0003Z"></path></svg>
+     </div>
       
       <div className="flex flex-col items-center md:items-end  space-y-8 md:space-y-10">
         
         <div>
           <div className="flex items-center justify-center">
             <div className="text-blue-500 mr-2 text-2xl" />
-            <h3 className="text-xl md:text-2xl lg:text-3xl font-medium">
+            <h3 className="text-xl md:text-2xl lg:text-3xl font-medium mb-3">
             Innovation
             </h3>
           </div>
-          <p className="text-gray-600 text-sm md:text-base lg:text-lg max-w-md mx-auto px-4 sm:px-0 justify-center opacity-80">
+          <p className="text-gray-600 text-sm md:text-base lg:text-base max-w-md mx-auto px-4 sm:px-0 justify-center opacity-90">
           We are committed to continuously improving our technology to meet the evolving needs of our users
           and stay ahead of industry trends.
           </p>
         </div>
-        
-        <div>
+         <div>
+          </div>
+
+
+         <div className="flex flex-col items-center">
+      <div className="flex items-center justify-center text-2xl font-normal w-16 h-16 mb-6 bg-blue-600 rounded-xl">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="rgba(255,255,255,1)"><path d="M20.0833 15.1999L21.2854 15.9212C21.5221 16.0633 21.5989 16.3704 21.4569 16.6072C21.4146 16.6776 21.3557 16.7365 21.2854 16.7787L12.5144 22.0412C12.1977 22.2313 11.8021 22.2313 11.4854 22.0412L2.71451 16.7787C2.47772 16.6366 2.40093 16.3295 2.54301 16.0927C2.58523 16.0223 2.64413 15.9634 2.71451 15.9212L3.9166 15.1999L11.9999 20.0499L20.0833 15.1999ZM20.0833 10.4999L21.2854 11.2212C21.5221 11.3633 21.5989 11.6704 21.4569 11.9072C21.4146 11.9776 21.3557 12.0365 21.2854 12.0787L11.9999 17.6499L2.71451 12.0787C2.47772 11.9366 2.40093 11.6295 2.54301 11.3927C2.58523 11.3223 2.64413 11.2634 2.71451 11.2212L3.9166 10.4999L11.9999 15.3499L20.0833 10.4999ZM12.5144 1.30864L21.2854 6.5712C21.5221 6.71327 21.5989 7.0204 21.4569 7.25719C21.4146 7.32757 21.3557 7.38647 21.2854 7.42869L11.9999 12.9999L2.71451 7.42869C2.47772 7.28662 2.40093 6.97949 2.54301 6.7427C2.58523 6.67232 2.64413 6.61343 2.71451 6.5712L11.4854 1.30864C11.8021 1.11864 12.1977 1.11864 12.5144 1.30864ZM11.9999 3.33233L5.88723 6.99995L11.9999 10.6676L18.1126 6.99995L11.9999 3.33233Z"></path></svg>
+      </div>
           <div className="flex items-center justify-center">
             <div className="text-blue-500 mr-2 text-2xl" />
-            <h3 className="text-xl md:text-2xl lg:text-3xl font-medium">
+            <h3 className="text-xl md:text-2xl lg:text-3xl font-medium mb-3">
             Simplicity
             </h3>
           </div>
-          <p className="text-gray-600 text-sm md:text-base lg:text-lg max-w-md mx-auto px-4 sm:px-0 opacity-80">
+          <p className="text-gray-600 text-sm md:text-base lg:text-base max-w-md mx-auto px-4 sm:px-0 opacity-90">
           We believe that great technology should be intuitive and easy to use. Our solutions are designed 
           to streamline your hiring process without unnecessary complexity.
           </p>
         </div>
       </div>
+      </div>
+
+      <div className="flex flex-col items-center">
+      <div className="flex items-center justify-center text-2xl font-normal w-16 h-16 mb-6 bg-blue-600 rounded-xl">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="rgba(255,255,255,1)"><path d="M14.5998 8.00033H21C22.1046 8.00033 23 8.89576 23 10.0003V12.1047C23 12.3659 22.9488 12.6246 22.8494 12.8662L19.755 20.3811C19.6007 20.7558 19.2355 21.0003 18.8303 21.0003H2C1.44772 21.0003 1 20.5526 1 20.0003V10.0003C1 9.44804 1.44772 9.00033 2 9.00033H5.48184C5.80677 9.00033 6.11143 8.84246 6.29881 8.57701L11.7522 0.851355C11.8947 0.649486 12.1633 0.581978 12.3843 0.692483L14.1984 1.59951C15.25 2.12534 15.7931 3.31292 15.5031 4.45235L14.5998 8.00033ZM7 10.5878V19.0003H18.1606L21 12.1047V10.0003H14.5998C13.2951 10.0003 12.3398 8.77128 12.6616 7.50691L13.5649 3.95894C13.6229 3.73105 13.5143 3.49353 13.3039 3.38837L12.6428 3.0578L7.93275 9.73038C7.68285 10.0844 7.36341 10.3746 7 10.5878ZM5 11.0003H3V19.0003H5V11.0003Z"></path></svg>
+     </div>
+     
       
       <div className="flex flex-col items-center">
-        
-
         <div className="text-center mb-4 md:mb-6">
           <div className="flex items-center justify-center">
             <div className="text-blue-500 mr-2 text-2xl" />
-            <h3 className="text-xl md:text-2xl lg:text-2xl mb-5 font-medium">
+            <h3 className="text-xl md:text-2xl lg:text-2xl font-medium mb-5">
             Customer Success
             </h3>
           </div>
-          <p className="text-gray-600 text-sm md:text-base lg:text-lg max-w-md mx-auto px-4 sm:px-0 justify-center opacity-80">
+          <p className="text-gray-600 text-sm mb-5 md:text-base lg:text-base max-w-md mx-auto px-4 sm:px-0 justify-center opacity-90">
           Your success is our success. We are devoted to providing exceptional support and resources to 
           help you achieve your hiring goals.
           </p>
         </div>
+        </div>
         
         {/* Center Image */}
-        <div className="flex justify-center  mb-4 md:mb-6">
-          <img
-            src="/image/img1.webp" 
-            alt="Central Illustration"
-            className="rounded-lg shadow-lg object-cover w-40 h-40 sm:w-48 sm:h-48 md:w-[30rem] md:h-[42rem]"
-          />
-        </div>
 
+        
+        <div className="flex justify-center mb-4 md:mb-6">
+          <img
+          src="/image/img1.webp"
+          alt="Central Illustration"
+           className="rounded-2xl shadow-lg object-cover w-80 h-40 sm:w-96 sm:h-48 md:w-[50rem] md:h-[42rem]"
+            />
+            </div>
+
+        
+
+        <div className="flex flex-col items-center">
+      <div className="flex items-center justify-center text-2xl font-normal w-16 h-16 mb-2 mt-3 bg-blue-600 rounded-xl">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="rgba(255,255,255,1)"><path d="M3.78307 2.82598L12 1L20.2169 2.82598C20.6745 2.92766 21 3.33347 21 3.80217V13.7889C21 15.795 19.9974 17.6684 18.3282 18.7812L12 23L5.6718 18.7812C4.00261 17.6684 3 15.795 3 13.7889V3.80217C3 3.33347 3.32553 2.92766 3.78307 2.82598ZM5 4.60434V13.7889C5 15.1263 5.6684 16.3752 6.7812 17.1171L12 20.5963L17.2188 17.1171C18.3316 16.3752 19 15.1263 19 13.7889V4.60434L12 3.04879L5 4.60434Z"></path></svg>
+      </div>
         <div className="text-center mt-4 md:mt-6">
           <div className="flex items-center justify-center">
             <div className="text-blue-500 mr-2 text-2xl" />
-            <h3 className="text-xl md:text-2xl lg:text-3xl font-medium ">
+            <h3 className="text-xl md:text-2xl lg:text-3xl font-medium mb-3">
             Security
             </h3>
           </div>
-          <p className="text-gray-600 text-sm md:text-base lg:text-lg max-w-md mx-auto px-4 sm:px-0 justify-center opacity-80">
+          <p className="text-gray-600 text-sm md:text-base lg:text-base max-w-md mx-auto px-4 sm:px-0 justify-center opacity-90">
           We prioritize the protection of your data with robust security measures. Our systems are designed to safeguard 
           your information and ensure privacy, giving you peace of mind.
           </p>
         </div>
       </div>
+      </div>
       
+
+      <div className="flex flex-col items-center gap-10">
+      <div className="flex items-center justify-center text-2xl font-normal w-16 h-16 mb-6 bg-blue-600 rounded-xl">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="rgba(255,255,255,1)"><path d="M12.0003 3C17.3924 3 21.8784 6.87976 22.8189 12C21.8784 17.1202 17.3924 21 12.0003 21C6.60812 21 2.12215 17.1202 1.18164 12C2.12215 6.87976 6.60812 3 12.0003 3ZM12.0003 19C16.2359 19 19.8603 16.052 20.7777 12C19.8603 7.94803 16.2359 5 12.0003 5C7.7646 5 4.14022 7.94803 3.22278 12C4.14022 16.052 7.7646 19 12.0003 19ZM12.0003 16.5C9.51498 16.5 7.50026 14.4853 7.50026 12C7.50026 9.51472 9.51498 7.5 12.0003 7.5C14.4855 7.5 16.5003 9.51472 16.5003 12C16.5003 14.4853 14.4855 16.5 12.0003 16.5ZM12.0003 14.5C13.381 14.5 14.5003 13.3807 14.5003 12C14.5003 10.6193 13.381 9.5 12.0003 9.5C10.6196 9.5 9.50026 10.6193 9.50026 12C9.50026 13.3807 10.6196 14.5 12.0003 14.5Z"></path></svg>
+      </div>
       <div className="flex flex-col items-center md:items-start space-y-8 md:space-y-10">
-        
-        {/* First Title-Description Pair */}
         <div>
           <div className="flex items-center justify-center">
             <div className="text-blue-500 mr-2 text-2xl" />
-            <h3 className="text-xl md:text-2xl lg:text-3xl font-medium">
+            <h3 className="text-xl md:text-2xl lg:text-3xl font-medium mb-3">
             Transparency
             </h3>
           </div>
-          <p className="text-gray-600 text-sm md:text-base lg:text-lg max-w-md mx-auto px-4 sm:px-0 justify-center opacity-80">
+          <p className="text-gray-600 text-sm md:text-base lg:text-base max-w-md mx-auto px-4 sm:px-0 justify-center opacity-90">
           We value open and honest communication, both within our team and with our users. We’re dedicated 
           to building trust through clarity and integrity.
           </p>
         </div>
+        </div>
         
         {/* Second Title-Description Pair */}
+
+        <div className="flex flex-col items-center">
+      <div className="flex items-center justify-center text-2xl font-normal w-16 h-16 mb-6 bg-blue-600 rounded-xl">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="rgba(255,255,255,1)"><path d="M2 22C2 17.5817 5.58172 14 10 14C14.4183 14 18 17.5817 18 22H16C16 18.6863 13.3137 16 10 16C6.68629 16 4 18.6863 4 22H2ZM10 13C6.685 13 4 10.315 4 7C4 3.685 6.685 1 10 1C13.315 1 16 3.685 16 7C16 10.315 13.315 13 10 13ZM10 11C12.21 11 14 9.21 14 7C14 4.79 12.21 3 10 3C7.79 3 6 4.79 6 7C6 9.21 7.79 11 10 11ZM18.2837 14.7028C21.0644 15.9561 23 18.752 23 22H21C21 19.564 19.5483 17.4671 17.4628 16.5271L18.2837 14.7028ZM17.5962 3.41321C19.5944 4.23703 21 6.20361 21 8.5C21 11.3702 18.8042 13.7252 16 13.9776V11.9646C17.6967 11.7222 19 10.264 19 8.5C19 7.11935 18.2016 5.92603 17.041 5.35635L17.5962 3.41321Z"></path></svg>
+      </div>
         <div>
           <div className="flex items-center justify-center">
             <div className="text-blue-500 mr-2 text-2xl" />
-            <h3 className="text-2xl md:text-2xl lg:text-3xl font-medium">
+            <h3 className="text-2xl md:text-2xl lg:text-3xl font-medium mb-3">
             Inclusivity
             </h3>
           </div>
-          <p className="text-gray-600 text-sm md:text-base lg:text-lg max-w-md mx-auto px-4 sm:px-0  opacity-80">
+          <p className="text-gray-600 text-sm md:text-base lg:text-base max-w-md mx-auto px-4 sm:px-0  opacity-90">
           We are committed to fostering a diverse and inclusive hiring environment. By eliminating biases 
           and ensuring fairness, we empower every candidate to present their unique strengths and capabilities.
           </p>
+          </div>
+          
         </div>
       </div>
     </div>
   </div>
-</section>);
+</section>
+);
 };
 
 
